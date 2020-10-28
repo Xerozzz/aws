fe546a934718d65b4e016430436069357d185013
Xerozzz <yiterng.19@ichat.sp.edu.sg>
2020-10-28 11:48:37 +0800

    Update describe all instances.sh

--- scripts/ec2/describe all instances.sh
+++ scripts/ec2/describe all instances.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#List Instances In Each Region.
-for region in $(aws ec2 describe-regions --output text | cut -f3)
-do
-     aws ec2 describe-instances --region "$region" --output text --query 'Reservations[*].Instances[*].[Placement.AvailabilityZone, InstanceId, InstanceType, ImageId, PublicIpAddress ]'
-done
+#List Instances In your Default Region.
+
+aws ec2 describe-instances --output text --query 'Reservations[*].Instances[*].[Placement.AvailabilityZone, InstanceId, InstanceType, ImageId, PublicIpAddress ]'
